64c45ea83dc96f65bada7dcb4cabef5ca5ad24e6
Dilip9 <dilip.verma940805@gmail.com>
2026-01-01 23:58:44 +0530

    Structure of the handler application

---
+++ src/main/java/gouri/notification/notification_handler/config/KafkaConfig.java
@@ -0,0 +1,4 @@
+package gouri.notification.notification_handler.config;
+
+public class KafkaConfig {
+}
---
+++ src/main/java/gouri/notification/notification_handler/service/SmsSenderService.java
@@ -0,0 +1,4 @@
+package gouri.notification.notification_handler.service;
+
+public class SmsSenderService {
+}
