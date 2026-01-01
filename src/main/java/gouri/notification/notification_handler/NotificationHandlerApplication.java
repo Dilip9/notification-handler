67bda5899bf98f652bdffa88a83415fc95dbde80
Dilip9 <dilip.verma940805@gmail.com>
2026-01-01 23:42:57 +0530

    Initial Commit

---
+++ src/main/java/gouri/notification/notification_handler/NotificationHandlerApplication.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gouri/notification/notification_handler/NotificationHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package gouri.notification.notification_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
