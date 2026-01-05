68e053cd93a947781347b70a8128a51ad0270bf5
Dilip9 <dilip.verma940805@gmail.com>
2026-01-06 00:14:42 +0530

    ResilienceConfig file completion

--- src/main/java/gouri/notification/notification_handler/config/ResilienceConfig.java
+++ src/main/java/gouri/notification/notification_handler/config/ResilienceConfig.java
@@ -1,7 +1,40 @@
 package gouri.notification.notification_handler.config;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.retry.RetryConfig;
+import io.github.resilience4j.retry.RetryRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.TimeoutException;
 
 @Configuration
 public class ResilienceConfig {
+
+    /***** Retry Configuration *****/
+    @Bean
+    public RetryConfig retryConfig() {
+        RetryConfig retryConfig = RetryConfig.custom()
+                .maxAttempts(3)
+                .waitDuration(Duration.ofMillis(500))
+                .retryExceptions(IOException.class, TimeoutException.class)
+                .build();
+        return (RetryConfig) RetryRegistry.of(retryConfig);
+    }
+
+    /***** Circuit Breaker Configuration *****/
+
+    @Bean
+    public CircuitBreakerRegistry circuitBreakerRegistry() {
+        CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
+                .failureRateThreshold(50)
+                .waitDurationInOpenState(Duration.ofMillis(1000))
+                .slidingWindowSize(10)
+                .minimumNumberOfCalls(5)
+                .build();
+        return CircuitBreakerRegistry.of(circuitBreakerConfig);
+    }
 }
